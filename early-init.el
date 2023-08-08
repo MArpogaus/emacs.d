c4026c828d13c0769469beac8e3b5bd9c4d549d4
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2023-08-08 11:17:09 +0200

    refactors startup optimization into early-init

--- early-init.el
+++ early-init.el
@@ -1,3 +1,69 @@
+;; If an `.el' file is newer than its corresponding `.elc', load the `.el'.
+(setq load-prefer-newer t)
+
+;; Disable certain byte compiler warnings to cut down on the noise.
+(setq byte-compile-warnings '(not free-vars unresolved noruntime lexical make-local))
+
+;; We're going to increase the gc-cons-threshold to a very high number to decrease the load time and add a hook to measure Emacs startup time.
+;; Taken from: https://github.com/nilcons/emacs-use-package-fast#a-trick-less-gc-during-startup
+
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+;; Let's lower our GC thresholds back down to a sane level.
+(add-hook 'after-init-hook (lambda ()
+                             ;; restore after startup
+                             (setq gc-cons-threshold (* 2 1000 1000)
+                                   gc-cons-percentage 0.1)))
+
+;; Profile emacs startup
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "*** Emacs loaded in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+
 ;; prevent package.el loading packages prior to their init-file loading.
-(eval-and-compile
-  (setq package-enable-at-startup nil))
+(setq package-enable-at-startup nil)
+
+;; The following optimisatzion have been taken from:
+;; https://github.com/mnewt/dotemacs/blob/master/early-init.el
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t
+      frame-resize-pixelwise t)
+
+;; Ignore X resources; its settings would be redundant with the other settings
+;; in this file and can conflict with later config (particularly where the
+;; cursor color is concerned).
+(advice-add #'x-apply-session-resources :override #'ignore)
+
+;; Unset `file-name-handler-alist' too (temporarily). Every file opened and
+;; loaded by Emacs will run through this list to check for a proper handler for
+;; the file, but during startup, it won’t need any of them.
+(defvar file-name-handler-alist-old file-name-handler-alist)
+(setq file-name-handler-alist nil)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq file-name-handler-alist file-name-handler-alist-old)))
+
+
+;; straight.el bootstrap code
+;;disable checking (for speedup).
+(setq straight-check-for-modifications nil)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
